5b9c21d76f1a4e0d275e62af9e17ed24ed39bcbc
BlackDow10 <andrew770534@gmail.com>
2019-10-28 23:25:27 +0300

    new

--- D2.9.py
+++ D2.9.py
@@ -1,5 +1,6 @@
+import os
 import logging  
-from bottle import Bottle, request, HTTPError
+from bottle import Bottle, request, HTTPError, run
 import sentry_sdk
 from sentry_sdk.integrations.bottle import BottleIntegration
 sentry_sdk.init("https://513b50091e11430c9426a6e29d98f6f9@sentry.io/1797787",  integrations=[BottleIntegration()])
@@ -18,4 +19,12 @@ def fail():
     return HTTPError(500) 
   
   
-app.run(host='localhost', port=8080)
+if os.environ.get("APP_LOCATION") == "heroku":
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        server="gunicorn",
+        workers=3,
+    )
+else:
+    app.run(host="localhost", port=8080, debug=True)
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python D2.9.py
